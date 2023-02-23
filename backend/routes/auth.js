665d0e65a0fc5b425cfbdadef0c78ee7483b451d
ManjiCoder <manjicoder@gmail.com>
2023-02-24 01:24:50 +0530

    Video - 49 ✔ Hashing Passwords using bcryptjs in NodeJs - ✔ Using jsonwebtoken in NodeJs - ✔

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable object-shorthand */
 /* eslint-disable quotes */
 const express = require("express");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const user = require("../models/User");
 
+const JWT_SECRET = "Manji_The_Mountain_Man-21";
 const router = express.Router();
 
 // Creating a User using: POST => "/api/createuser". No login required
@@ -24,24 +29,35 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // To check whether User with this email exists
-    const duplicateEmail = await User.findOne({ email: req.body.email });
-    // console.log(duplicateEmail);
-    if (duplicateEmail) {
-      return res
-        .status(400)
-        .json({ error: "Sorry user with this email is already exists." });
-    }
-    // Create a new user & Saving User to the MongoDB
     try {
+      // To check whether User with this email exists
+      const duplicateEmail = await User.findOne({ email: req.body.email });
+      // console.log(duplicateEmail);
+      if (duplicateEmail) {
+        return res
+          .status(400)
+          .json({ error: "Sorry user with this email is already exists." });
+      }
+      // Hash Password & Adding Salt
+      const salt = await bcrypt.genSalt(10);
+      const securePassword = await bcrypt.hash(req.body.password, salt);
+      // Create a new user & Saving User to the MongoDB
       const newUser = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: securePassword,
       });
       // const newUser = await User(req.body).save();
       console.log("User saved to the MongoDB Successfully.");
-      res.json(newUser);
+      const data = {
+        user: {
+          id: newUser.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      // res.json(newUser);
+      console.log({ data, authToken });
+      res.json({ authToken });
     } catch (err) {
       console.log(err.message);
       res.status(500).json({
